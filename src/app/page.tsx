90234770bb9884aaa59d01f842e986ad390815a6
Saif Metkari <saifmetkri17@gmail.com>
2026-01-15 14:08:43 +0530

    Small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
     <div className="relative">
       <FloatingPetals />
       <HeroSection />
+      
       <ServicesSection />
       <FeaturedSection />
       <StorySection />
